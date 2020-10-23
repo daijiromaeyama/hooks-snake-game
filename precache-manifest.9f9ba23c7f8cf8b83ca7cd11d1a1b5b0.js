b5eca8fc2b71108288168f3a4843ca758410502a
daijiromaeyama <daijiro.maeyama13@gmail.com>
2020-10-23 20:58:32 +0900

    Updates

---
+++ precache-manifest.9f9ba23c7f8cf8b83ca7cd11d1a1b5b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31d14d1c8d2712f12c73876b36442ed0",
+    "url": "/hooks-snake-game/index.html"
+  },
+  {
+    "revision": "1acd32c63207652ebb0f",
+    "url": "/hooks-snake-game/static/css/main.2012d761.chunk.css"
+  },
+  {
+    "revision": "49efbe7a10a13a11a309",
+    "url": "/hooks-snake-game/static/js/2.af9040f3.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/hooks-snake-game/static/js/2.af9040f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acd32c63207652ebb0f",
+    "url": "/hooks-snake-game/static/js/main.b9b1e971.chunk.js"
+  },
+  {
+    "revision": "6a57cbb490b16b09b58d",
+    "url": "/hooks-snake-game/static/js/runtime-main.15d93ae8.js"
+  }
+]);
